3e19c74a80c9bfdd8dca288cb900cf84e90d2aac
Deepesh Malviya <66750993+1234567890970@users.noreply.github.com>
2023-12-01 12:43:27 +0000

    Variables let,const,var

---
+++ basics-01/01_variable.js
@@ -0,0 +1,13 @@
+const accountId = 123456;
+var accountPassword = "23456" ;
+let accountEmail = "dheeraj@gogl.com";
+accountCity = "raighar";
+
+// accountId = 3 ;// Not allowed
+accountPassword = "987654";
+accountEmail = "hero@hc.com";
+accountCity = "heydrabad"
+
+console.log(accountId);
+
+console.table([accountEmail, accountId, accountPassword, accountCity] )
