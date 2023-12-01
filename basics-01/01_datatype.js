c6f29628fdeb5e91e4323f9abe75afb2cba6eb2f
Deepesh Malviya <66750993+1234567890970@users.noreply.github.com>
2023-12-01 13:06:00 +0000

    discussed datatypes

---
+++ basics-01/01_datatype.js
@@ -0,0 +1,22 @@
+"use strict" // treate all code as newer version of JS
+
+// alert(3 + 3) // we are using node.js,  not browser
+
+// console.log( 3 
+//         +
+//          3 ); // code readability should be hifgher 
+
+let name = "Deepesh";
+let age = 23;
+let isLoggedIn = false;
+
+// number => 2 to power 53
+//bigint
+// boolean => true?false
+//null => stand alone value
+//undefined => value is not asigned
+//symbol => unique
+
+// object 
+console.log(typeof null); //object
+console.log(typeof undefined); //undefined
