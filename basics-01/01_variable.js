8b2881aaaca38af9a2428617c5e12cc9273eda05
Deepesh Malviya <66750993+1234567890970@users.noreply.github.com>
2023-12-01 12:50:06 +0000

    notes var not prefered

--- basics-01/01_variable.js
+++ basics-01/01_variable.js
@@ -2,12 +2,19 @@ const accountId = 123456;
 var accountPassword = "23456" ;
 let accountEmail = "dheeraj@gogl.com";
 accountCity = "raighar";
+let accountState;
 
 // accountId = 3 ;// Not allowed
+
 accountPassword = "987654";
 accountEmail = "hero@hc.com";
 accountCity = "heydrabad"
 
 console.log(accountId);
 
-console.table([accountEmail, accountId, accountPassword, accountCity] )
+/*
+prefer not to use {var}
+because of issue in block scope and functional scope
+*/
+
+console.table([accountEmail, accountId, accountPassword, accountCity, accountState] )
